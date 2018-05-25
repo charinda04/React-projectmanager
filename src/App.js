3e857c8ea19afaf6564701a857f802d05b1eb598
charinda04 <charinda04@gmail.com>
2018-05-25 20:12:05 +0530

    displayed an array on front page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects';
+
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      projects: [
+        {
+          title: 'Business Website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Moblie Development'
+        },
+        {
+          title: 'Ecommerce Shopping cart',
+          category: 'Web Development'
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        My App
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
